b522485da293bf64514c4c98e877fea9719c5268
Eric Dowell <eric@ericdowell.com>
2018-01-28 16:37:48 -0600

    Update gulpfile and spacing in style.scss.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -5,24 +5,31 @@ import concat from 'gulp-concat';
 import sourcemaps from 'gulp-sourcemaps';
 import uglify from 'gulp-uglify';
 
-const jsFiles = [
-    './node_modules/popper.js/dist/umd/popper.js',
-    './node_modules/jquery/dist/jquery.slim.js',
-    './node_modules/bootstrap/dist/js/bootstrap.js'
-];
+const sassConfig = {
+    source: './node_modules/bootstrap/scss/**/**/*',
+    destination: './_sass/bootstrap/'
+}, jsConfig = {
+    destination: './assets/js/',
+    source: [
+        './node_modules/popper.js/dist/umd/popper.js',
+        './node_modules/jquery/dist/jquery.slim.js',
+        './node_modules/bootstrap/dist/js/bootstrap.js'
+    ],
+    output: 'scripts.min.js'
+};
 
 gulp.task('sass', () => {
-    return gulp.src('./node_modules/bootstrap/scss/**/**/*')
-      .pipe(gulp.dest('./_sass/bootstrap/'));
+    return gulp.src(sassConfig.source)
+      .pipe(gulp.dest(sassConfig.destination));
 });
 
 gulp.task('js', () => {
-    return gulp.src(jsFiles)
+    return gulp.src(jsConfig.source)
       .pipe(sourcemaps.init())
-      .pipe(concat('scripts.min.js'))
+      .pipe(concat(jsConfig.output))
       .pipe(uglify())
       .pipe(sourcemaps.write('.'))
-      .pipe(gulp.dest('./assets/js/'));
+      .pipe(gulp.dest(jsConfig.destination));
 });
 
 gulp.task('default', ['js', 'sass']);
