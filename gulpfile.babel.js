58c252b73c219f829388526de07eff94a79e74a8
Eric Dowell <eric.dowell@startribune.com>
2018-01-28 16:43:43 -0600

    Use Gulp to Build Assets (#8)
    * Use gulp to build js files, update contact.
    
    * Update gulpfile and spacing in style.scss.
    
    * Update key in jsConfig in gulpfile.
    
    * Run npm build script in ci.

---
+++ gulpfile.babel.js
@@ -0,0 +1,35 @@
+'use strict';
+
+import gulp from 'gulp';
+import concat from 'gulp-concat';
+import sourcemaps from 'gulp-sourcemaps';
+import uglify from 'gulp-uglify';
+
+const sassConfig = {
+    source: './node_modules/bootstrap/scss/**/**/*',
+    destination: './_sass/bootstrap/'
+}, jsConfig = {
+    destination: './assets/js/',
+    file: 'scripts.min.js',
+    source: [
+        './node_modules/popper.js/dist/umd/popper.js',
+        './node_modules/jquery/dist/jquery.slim.js',
+        './node_modules/bootstrap/dist/js/bootstrap.js'
+    ]
+};
+
+gulp.task('sass', () => {
+    return gulp.src(sassConfig.source)
+      .pipe(gulp.dest(sassConfig.destination));
+});
+
+gulp.task('js', () => {
+    return gulp.src(jsConfig.source)
+      .pipe(sourcemaps.init())
+      .pipe(concat(jsConfig.file))
+      .pipe(uglify())
+      .pipe(sourcemaps.write('.'))
+      .pipe(gulp.dest(jsConfig.destination));
+});
+
+gulp.task('default', ['js', 'sass']);
