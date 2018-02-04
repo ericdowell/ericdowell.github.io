dc6c30a459c809767d3ced4f1a10cc56fbc883ec
Eric Dowell <eric.dowell@startribune.com>
2018-02-03 19:01:12 -0600

    Update build and test (#12)
    * Add more gulp libs and update ci command for testing build.
    
    * Move around json keys.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,7 +1,9 @@
 'use strict';
 
 import gulp from 'gulp';
+import beautify from 'gulp-beautify';
 import concat from 'gulp-concat';
+import gulpif from 'gulp-if';
 import sourcemaps from 'gulp-sourcemaps';
 import uglify from 'gulp-uglify';
 
@@ -9,16 +11,16 @@ const sassConfig = {
     source: './node_modules/bootstrap/scss/**/**/*',
     destination: './_sass/bootstrap/'
 }, jsConfig = {
-    destination: './assets/js/',
     file: 'scripts.min.js',
     source: [
         './node_modules/popper.js/dist/umd/popper.js',
         './node_modules/jquery/dist/jquery.slim.js',
         './node_modules/bootstrap/dist/js/bootstrap.js'
-    ]
-};
+    ],
+    destination: './assets/js/'
+}, isProd = (process.env.NODE_ENV === 'prod' || process.env.NODE_ENV === 'production');
 
-gulp.task('sass', () => {
+gulp.task('copy-sass', () => {
     return gulp.src(sassConfig.source)
       .pipe(gulp.dest(sassConfig.destination));
 });
@@ -27,9 +29,9 @@ gulp.task('js', () => {
     return gulp.src(jsConfig.source)
       .pipe(sourcemaps.init())
       .pipe(concat(jsConfig.file))
-      .pipe(uglify())
+      .pipe(gulpif(isProd, uglify(), beautify()))
       .pipe(sourcemaps.write('.'))
       .pipe(gulp.dest(jsConfig.destination));
 });
 
-gulp.task('default', ['js', 'sass']);
+gulp.task('default', ['js', 'copy-sass']);
